ba98ba2ec11ca27ad01dd8f142c3221bc629f251
saberfig <1023736798@qq.com>
2020-04-07 10:28:52 +0800

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,8 @@ class NewVisitorTest(unittest.TestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
+        # self.browser.get('http://127.0.0.1:8000')
+        
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
