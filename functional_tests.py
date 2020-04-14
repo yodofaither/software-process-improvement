f5ab28d9250f65bde5517e5e95000baad85fab9b
saberfig <1023736798@qq.com>
2020-04-14 10:58:03 +0800

    Front page HTML now generated from atemplate

--- functional_tests.py
+++ functional_tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
         
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
