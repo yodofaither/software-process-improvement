97f0e567234c843a928ea6b66b4ff5238b1e0119
saberfig <1023736798@qq.com>
2020-05-12 16:16:37 +0800

    make functional_tests an app, use LiveServerTestCase

---
+++ functional_tests/tests.py
@@ -0,0 +1,73 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+from selenium.webdriver.common.keys import Keys
+import time
+
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser=webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        # self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
+        
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
+        
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feather to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
+        
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feather to make a fly')
+        
+        self.fail('Finish the test!')
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+# assert 'To-Do' in browser.title,"Browser title was"+browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "But peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures )
+
+# when she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly"(Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep 
+
+# browser.quit()
